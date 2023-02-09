97c9cbb985077ef80e146c7a74716b3d027a444a
edualcelu <alexliga1998@outlook.com>
2023-02-09 18:08:26 -0500

    ok

---
+++ OperacionesMenuGitBisect/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionesMenuGitBisect
+{
+    internal class Class1
+    {
+    }
+}
